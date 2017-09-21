3244381b532e8766aa37f2efd8170230e92804dd
Ivan Frias <frias.ivan@gmail.com>
2017-09-22 00:04:01 +0100

    Added historic to historic.json file

--- views/historic/historial_2017.php
+++
@@ -1,9 +0,0 @@
-<?php include 'header.php';?>
-
-<ul class="breadcrumb">
-  <li><a href="index.php">Home</a></li>
-  <li>Historial</li>
-  <li>2017</li>
-</ul>
-									
-<?php include 'footer.php';?>
--- views/marathon/index.php
+++ views/marathon/index.php
@@ -5,6 +5,8 @@
 
 <p>A primeira meia maratona de teatro realizou-se no ano de 2006. A Meia maratona de teatro é um projeto que pretende abranger, entre outros e além de espetáculos de teatro, programação de música, dança, hora de conto, mostra de vídeos. É realizada durante três dias, em que o principal conceito é o de serem apresentados espetáculos com cerca de 3 horas de intervalo. Promover o intercâmbio e o conhecimento de novos grupos e trabalhos é também um dos objetivos. Desde 2014 que a Meia Maratona se estendeu à Vila Medieval de Ourém.</p>
 
+
+<!--
 <ul>
 	<li><a href="#">1ª Meia Maratona de Teatro – 8 e 9 de dezembro de 2006</a></li>
 	<li><a href="#">2ª Meia Maratona de Teatro – 1 a 14 de dezembro de 2007</a></li>
@@ -17,6 +19,12 @@
 	<li><a href="#">9ª Meia Maratona de Teatro - 10, 11 e 12 de julho de 2015</a></li>
 	<li><a href="#">10ª Meia Maratona de Teatro - 8, 9 e 10 de julho de 2016</a></li>
 </ul>
+
+-->
+<?php>
+
+
+<?php>
 
 <!-->
 							  <div class="w3-dropdown-hover">
