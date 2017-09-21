b0af476caf71057d3ee6800d6b9115e403dde4f8
Ivan Frias <frias.ivan@gmail.com>
2017-09-21 02:19:20 +0100

    added historic data for 2008, 2008 and 2010

---
+++ views/historic/.php
@@ -0,0 +1,125 @@
+{
+	"title":"O Gato (estreia)",
+	"site" :"Cenourém, Ourém",
+	"date" :"25 de novembro de 2010"
+},
+{
+	"title":"O Gato",
+	"site" :"Cenourém, Ourém",
+	"date" :"26 de novembro de 2010"
+},
+{
+	"title":"Halloween",
+	"site" :"Arte Caffé",
+	"date" :"31 de outubro de 2010"
+},
+{
+	"title":"Comemorações do Centenário da República",
+	"site" :"Ourém",
+	"date" :"5 de outubro de 2010"
+},
+{
+	"title":"Que Farei com este livro?", de José Saramago",
+	"site" :"Torreão – Castelo de Ourém",
+	"date" :"2 de outubro de 2010"
+},
+{
+	"title":"Que Farei com este livro?", de José Saramago",
+	"site" :"Torreão – Castelo de Ourém",
+	"date" :"26 de setembro de 2010"
+},
+{
+"title":"Que Farei com este livro?", de José Saramago",
+"site" :"Torreão – Castelo de Ourém",
+"date" :"25 de setembro de 2010"
+},
+{
+"title":"Na Terra dos Sonhos",
+"site" :"Museu Municipal de Ourém",
+"date" :"28 de agosto  de 2010"
+},
+{
+"title":"Na Terra dos Sonhos",
+"site" :"Museu Municipal de Ourém",
+"date" :"14 de agosto de 2010"
+},
+{
+"title":"Pic nic do Apollo",
+"site" :"Fontainhas da Atouguia",
+"date" :"30 de maio de 2010"
+},
+{
+"title":"Que Farei com este livro? De José Saramago",
+"site" :"Torreão – Castelo de Ourém",
+"date" :"15 de maio de 2010"
+},
+{
+"title":"O cliente nunca tem razão/Vende-se coração",
+"site" :"Associação Recreativa de Aboboreiras ",
+"date" :"8 de maio de 2010"
+},
+{
+"title":"Que Farei com este livro? De José Saramago",
+"site" :"Torreão – Castelo de Ourém",
+"date" :"1 de maio de 2010"
+},
+{
+"title":"Que Farei com este livro? De José Saramago",
+"site" :"Torreão – Castelo de Ourém",
+"date" :"24 de abril de 2010"
+},
+{
+"title":"Poesia ao Luar",
+"site" :"Comemorações do 25 de abril (Feira do Livro de 19 a 25 de abril) ",
+"date" :"23 de abril  de 2010"
+},
+{
+"title":"Leandro o Rei de Helíria",
+"site" :"O espetáculo foi realizado no âmbito da Semana da Leitura, assinalada de 17 a 24 de Abril, em Leiria (com a presença da Associação de Surdos da Alta Estremadura)",
+"date" :"17 de abril de 2010"
+},
+{
+"title":"Que Farei com este livro de José Saramago",
+"site" :"Torreão - Castelo de Ourém",
+"date" :"10 de abril de  2010"
+},
+{
+"title":"Comemoração do Dia Mundial do Teatro - apresentação de poemas de Fernando Pessoa e os seus heterónimos e Elsa",
+"site" :"-",
+"date" :"27 de março de 2010"
+},
+{
+"title":"Que Farei com este livro de José Saramago",
+"site" :"Torreão - Castelo de Ourém",
+"date" :"27 de março de 2010"
+},
+{
+"title":"O Cliente Nunca tem razão e vende-se coração",
+"site" :"TASE, Santa Eufémia, Leiria",
+"date" :"28 de março de 2010"
+},
+{
+"title":"Que Farei com este livro de José Saramago",
+"site" :"Torreão - Castelo de Ourém",
+"date" :"20 de março de 2010"
+},
+{
+"title":"A Derrota do Kid Labaredas",
+"site" :"Mata Municipal de Ourém - o Dia Mundial do Ambiente",
+"date" :"21 de Março de 2010"
+},
+{
+"title":"Na Terra dos Sonhos",
+"site" :"Atouguia – ARCA",
+"date" :"23 de Janeiro de 2010"
+},
+{
+"title":"Jantar de Reis",
+"site" :"-",
+"date" :"9 de janeiro de 2010"
+},
+{
+"title":"4ª Meia Maratona de Teatro",
+"site" :"-",
+"date" :"4 e 5 de dezembro de 2010"
+}
